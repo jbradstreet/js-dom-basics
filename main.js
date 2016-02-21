d0d3d0137b7bab7008586eb5ac4413ba3a376f9e
Jenni Bradstreet <jennic15@gmail.com>
2016-02-21 02:17:41 -0800

    Style nav footer

---
+++ main.js
@@ -0,0 +1,34 @@
+// var firstParagraph = document.getElementById('main');
+// console.log(firstParagraph);
+
+// var ulElements = document.getElementsByTagName('ul');
+// console.log(ulElements);
+
+// var ulElement = document.getElementsByTagName('ul')[0];
+// console.log(ulElement);
+
+// var allListItems = document.getElementsByTagName('li');
+// console.log(allListItems);
+
+// use this to find out how many list items there are before targeting
+// var allListItems = document.getElementsByTagName('li');
+// for (var i = 0; i < allListItems.length; i++) {
+//   console.log(allListItems[i]);
+// }
+//
+// var evens = document.getElementsByClassName('even');
+// console.log(evens);
+//
+// var odds = document.getElementsByClassName('odd');
+// console.log(odds);
+//
+// console.log(evens[3]);
+
+// var main = document.querySelector('#main');
+// console.log(main);
+
+var firstOddElement = document.querySelector('.odd');
+console.log(firstOddElement);
+
+// var oddAndEvenElements = document.querySelectorAll('.odd, .even');
+// console.log(oddAndEvenElements);
